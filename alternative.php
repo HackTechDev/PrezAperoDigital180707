33f7509321e9cef936c49f81c23e45c66ecbf2c9
samuel.gondouin <samuel.gondouin@gandi.net>
2018-07-06 15:51:53 +0200

    Retab slide

--- alternative.php
+++ alternative.php
@@ -3,11 +3,21 @@
                     <h1>Les alternatives</h1>
                     <p>
                         <ul>
-                            <li>LattePanda</li>
-                            <li>Cubieboard</li>
-                            <li>pcDuino</li>
-                            <li>Banana Pi</li>
-                            <li>BeagleBone</li>
+                            <li>LattePanda<br/>
+                             <img src="image/.jpg" width="50%">
+                            </li>
+                            <li>Cubieboarda<br/>
+                             <img src="image/.jpg" width="50%">
+                            </li>
+                            <li>pcDuinoa<br/>
+                             <img src="image/.jpg" width="50%">
+                            </li>
+                            <li>Banana Pia<br/>
+                             <img src="image/.jpg" width="50%">
+                            </li>
+                            <li>BeagleBonea<br/>
+                             <img src="image/.jpg" width="50%">
+                            </li>
                         </ul>
                     </p>
                 </section>
--- autremodele.php
+++ autremodele.php
@@ -3,7 +3,9 @@
                     <h1>Les autres modéles de Raspberry Pi</h1>
                     <p>
                         <ul>
-                            <li>Raspberry Pi Zero WH</li>
+                            <li>Raspberry Pi Zero WH<br/>
+                            <img src="image/.jpg" width="50%">	
+                            </li>
                         </ul>
                     </p>
                 </section>
